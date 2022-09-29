6b842847d9a274aabe2c841b1d673fd43d61b48d
lwhitmo <lwhitmo03@charlottesvilleschools.org>
2022-09-29 11:11:36 -0400

    lcd prints numbers with the push of a button

--- Code/lcdcodetake2.py
+++ Code/lcdcodetake2.py
@@ -1,12 +1,12 @@
-##original code credits go to Graham GS
-## view linked code https://github.com/VeganPorkChop/CircutPython/blob/master/LCD.py 
+#original code credits go to Graham GS view linked code https://github.com/VeganPorkChop/CircutPython/blob/master/LCD.py 
 
 import board
+import time
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 from digitalio import DigitalInOut, Direction, Pull
 
-# get and i2c object!!
+# get and i2c object
 i2c = board.I2C()
 btn = DigitalInOut(board.D3)
 btn2 = DigitalInOut(board.D2)
@@ -18,20 +18,35 @@ btn2.pull = Pull.UP
 lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
 cur_state = True
 prev_state = True
+cur_state2 = True
+prev_state2 = True
 buttonPress = 0
-
+ 
 while True:
-    if btn2 == True:
-        lcd.print("BTN is down")
+    while btn2.value == True:
         cur_state = btn.value
         if cur_state != prev_state:
             if not cur_state:
                 buttonPress = buttonPress + 1
                 lcd.clear()
-                lcd.set_cursor_pos(1, 0)
-                lcd.print("BTN Press:")
+                lcd.set_cursor_pos(0,0)
                 lcd.print(str(buttonPress))
+            else:
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+                print(buttonPress)
         prev_state = cur_state
     else:
-        lcd.set_cursor_pos(0, 0)
-        lcd.print("BTN is up")
+        cur_state2 = btn.value
+        if cur_state2 != prev_state2:
+            if not cur_state2:
+                buttonPress = buttonPress - 1
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+            else:
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+        prev_state2 = cur_state2
